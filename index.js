7833c1cb69c3574a6885cfc0d25e4f7b2b74da79
Prof. Doo <prof.stefano.durso@gmail.com>
2020-10-06 15:52:09 +0000

    process.env.port setting

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const port = process.env.PORT || 5000
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Express app!')
+  res.send('Hello world!')
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('server started');
 });
